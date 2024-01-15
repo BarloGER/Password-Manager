b9e4904ba2e1e1a869418155f078911ae8c55b76
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-01-15 15:52:47 +0100

    Client: Added first dashboard design

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,16 +1,25 @@
 import { Link } from "react-router-dom";
+import "../assets/dashboard.css";
 
 const Dashboard = () => {
   return (
-    <>
-      <h1>Dashboard</h1>
-      <Link to="/auth/accounts">
-        <button>Accounts</button>
-      </Link>
-      <Link to="/auth/profile">
-        <button>Profil</button>
-      </Link>
-    </>
+    <section className="dashboard-container">
+      <h1 className="dashboard-title">Dashboard</h1>
+      <div className="dashboard-cards">
+        <Link to="/auth/profile" className="dashboard-card">
+          <button>Profil</button>
+        </Link>
+        <Link to="/auth/accounts" className="dashboard-card">
+          <button>Accounts</button>
+        </Link>
+        <Link to="/auth/notes" className="dashboard-card">
+          <button>Notizen</button>
+        </Link>
+        <Link to="/auth/generator" className="dashboard-card">
+          <button>Passwort Generator</button>
+        </Link>
+      </div>
+    </section>
   );
 };
 
