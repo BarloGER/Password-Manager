98632f5cf214cf47e371ea09cccf5f3f46599d52
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-01-17 17:41:15 +0100

    Server: Improved errorHandler

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,9 +1,9 @@
 const errorHandler = (err, req, res, next) => {
   res.status(err.statusCode || 500).json({
-    error: err.message,
+    message: err.message,
     errorType: err.errorType,
     errorCode: err.errorCode,
-    // statusCode: err.statusCode,
+    statusCode: err.statusCode,
   });
 };
 
